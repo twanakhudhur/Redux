dc9cb0efeedc0fe9450a9d4870691e0afed9be27
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-09-16 00:09:15 +0300

    Simple Redux toolkit todo

---
+++ reduxtoolkittodolist/src/App.jsx
@@ -0,0 +1,9 @@
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+
+export default function App() {
+  return <>
+   <AddTodo />
+   <TodoList />
+  </>;
+}
---
+++ reduxtoolkittodolist/src/features/todo/todoSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        title: action.payload,
+        completed: false,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    toggleComplete: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleComplete } = todoSlice.actions;
+export default todoSlice.reducer;
